502836f603b839199176c473e72cef612f88e9f0
Priyansh Kabra <priyanshkabra2020@gmail.com>
2026-09-01 20:49:09 +0530

    Add Project 4 Jenkins scale setup

---
+++ Project4_Jenkins_Scale/maven-project/src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+    public static String message() {
+        return "Distributed Jenkins Maven Pipeline";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(message());
+    }
+}
